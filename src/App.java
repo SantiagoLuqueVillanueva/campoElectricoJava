c9c128fd5f6c384bcb5c279cdaab2b1c6e11e41e
Santiago Luque Villanueva <sluqvii2906@cpifpalanturing.es>
2025-11-13 14:04:35 +0100

    Correcion de nombre de variables y test de la fórmula

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static final String CLEAN_SCREEN = "\033[H\033[2J";
     public static void main(String[] args) throws Exception {
         int x = 0;
-        long millies = 350;
+        long millis = 350;
         double q = 2.0;
         double E = 5.0;
-        double m = 1.0;
+        double m = 6.0;
 
         double a = (q*E)/m;
 
@@ -15,10 +15,10 @@ public class App {
             }
             System.out.print("*");
             x++;
-            millies -= a;
-            if(millies < 20)
-                millies = 20;
-            Thread.sleep(millies);
+            millis -= a;
+            if(millis < 20)
+                millis = 20;
+            Thread.sleep(millis);
             System.out.print(CLEAN_SCREEN);
         } while (true);
     }
